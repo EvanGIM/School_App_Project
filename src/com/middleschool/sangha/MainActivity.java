a3cbc02ce4eb654f57848853f406a8d06595c13d
EvanGIM <knya1234@gmail.com>
2014-08-31 17:27:12 +0900

    BEAT 3
    상하중학교 어플 구현
    1 학교 홈페이지 접속
    2 학교 급식 파싱
    <추가할것>
    3.일정
    4.UI개선
    5.학교 정보
    6.스플래시

--- src/com/middleschool/sangha/MainActivity.java
+++ src/com/middleschool/sangha/MainActivity.java
@@ -20,38 +20,31 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.main);
         
         button1 = (Button) findViewById(R.id.button1);
-        button2 = (Button) findViewById(R.id.button2);
         button1.setOnClickListener(listener);
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(listener);
     }
     
         Button.OnClickListener listener = new Button.OnClickListener()
          {
           public void onClick(View v)
            {
-        	switch(v.getId()){
-       	    case R.id.button1:
-        	Toast.makeText(getApplicationContext(), "학교 홈페이지로 이동합니다.",
-        			Toast.LENGTH_LONG).show(); }
-        		
-        	switch(v.getId()){
-     	    case R.id.button1:
-      		    Intent myIntent = new Intent(MainActivity.this, Webview.class);
-        		startActivity(myIntent);
-        	break;
-        	}
-        	button2.setOnClickListener(listener);
-        	
-        	switch(v.getId()){
-     	    case R.id.button2:
-        	Toast.makeText(getApplicationContext(), "급식 엑티비티로 이동합니다.",
-        			Toast.LENGTH_LONG).show(); }
-        		
-        	switch(v.getId()){
-     	    case R.id.button2:
-      		    Intent myIntent = new Intent(MainActivity.this, com.middleschool.sangha.bap.Bap.class);
-        		startActivity(myIntent);
-        	break;
-        	}
+        	switch (v.getId()) {
+        	case R.id.button1:
+        	    Toast.makeText(getApplicationContext(), "학교 홈페이지로 이동합니다.",
+        	        Toast.LENGTH_LONG).show();
+        	    Intent myIntent = new Intent(MainActivity.this, Webview.class);
+        	    startActivity(myIntent);
+        	    break;
+
+
+            case R.id.button2:
+        	    Toast.makeText(getApplicationContext(), "급식 엑티비티로 이동합니다.",
+        	        Toast.LENGTH_LONG).show();
+        	    Intent mIntent = new Intent(MainActivity.this, com.middleschool.sangha.bap.Bap.class);
+        	    startActivity(mIntent);
+        	    break;
+            }
         }
     };
 
